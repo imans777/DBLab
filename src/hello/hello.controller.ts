9fbf219dc3b6d5ce880e44433fd844ff381fa214
imans777 <iman.s_sani@yahoo.com>
2021-04-02 08:38:21 +0430

    comment out warm up codes (not removing it yet because it can be my reference)

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -7,30 +7,31 @@ import { HelloService } from './hello.service';
 export class HelloController {
   constructor(private readonly helloService: HelloService) {}
 
-  @ApiResponse({ status: 200, description: 'say hello!' })
-  @Post('welcome')
-  @Header('Content-Type', 'application/json')
-  async sayWelcome(@Body() personDTO: PersonDTO): Promise<{ data: string }> {
-    return {
-      data: await this.helloService.welcome(personDTO),
-    };
-  }
+  // WARM UP CODES --->
+  //   @ApiResponse({ status: 200, description: 'say hello!' })
+  //   @Post('welcome')
+  //   @Header('Content-Type', 'application/json')
+  //   async sayWelcome(@Body() personDTO: PersonDTO): Promise<{ data: string }> {
+  //     return {
+  //       data: await this.helloService.welcome(personDTO),
+  //     };
+  //   }
 
-  @ApiResponse({ status: 200 })
-  @ApiQuery({ name: 'name', required: true, type: String })
-  @ApiQuery({
-    name: 'year',
-    required: false,
-    type: Number,
-    description: 'you can ignore this',
-  })
-  @Get('welcome')
-  async sayWelcome2(
-    @Query('name') iName,
-    @Query('year') iYear,
-  ): Promise<{ data: string }> {
-    return {
-      data: await this.helloService.welcome({ name: iName, year: iYear }),
-    };
-  }
+  //   @ApiResponse({ status: 200 })
+  //   @ApiQuery({ name: 'name', required: true, type: String })
+  //   @ApiQuery({
+  //     name: 'year',
+  //     required: false,
+  //     type: Number,
+  //     description: 'you can ignore this',
+  //   })
+  //   @Get('welcome')
+  //   async sayWelcome2(
+  //     @Query('name') iName,
+  //     @Query('year') iYear,
+  //   ): Promise<{ data: string }> {
+  //     return {
+  //       data: await this.helloService.welcome({ name: iName, year: iYear }),
+  //     };
+  //   }
 }
