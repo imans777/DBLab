5238b3c68669ace567cdf907cf51ccea9f59ac5d
imans777 <iman.s_sani@yahoo.com>
2021-04-02 09:16:55 +0430

    create user/genre/book files

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { GenreModule } from './genre/genre.module';
+import { BookModule } from './book/book.module';
 
 @Module({
-  providers: [BooksService],
-  controllers: [BooksController]
+  providers: [BooksService, UserService],
+  controllers: [BooksController, UserController],
+  imports: [UserModule, GenreModule, BookModule],
 })
 export class BooksModule {}
