1e82e4f72466d865949597c8c10ab5c440a5ce32
imans777 <iman.s_sani@yahoo.com>
2021-04-09 17:50:10 +0430

    create employee/employer/project modules/controller/services

--- src/jobseekers/jobseekers.module.ts
+++ src/jobseekers/jobseekers.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JobseekersService } from './jobseekers.service';
 import { JobseekersController } from './jobseekers.controller';
+import { ProjectModule } from './project/project.module';
+import { EmployeeModule } from './employee/employee.module';
+import { EmployerModule } from './employer/employer.module';
 
 @Module({
   providers: [JobseekersService],
   controllers: [JobseekersController],
+  imports: [ProjectModule, EmployeeModule, EmployerModule],
 })
 export class JobseekersModule {}
